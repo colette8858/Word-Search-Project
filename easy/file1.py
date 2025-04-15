545d3c119bd050c232be9d63f3d11261700df83f
Maya Kjos <mkjos001@student.franciscan.edu>
2025-04-15 12:12:23 -0400

    save

--- easy/file1.py
+++ easy/file1.py
@@ -1,5 +1,25 @@
 import tkinter as tk
 
+window=tk.Tk()
+window.title("Word Search Grid")
+window.config(bg="white")
+
+frm_b=tk.Frame(window, width=100, height=100, bg="pink")
+frm_b.pack(padx=10, pady=10)
+
+label=tk.Label(frm_b, text="", width=100, height=50, bg="white", relief="raised")
+label.pack(padx=5, pady=5)
+
+
+
+
+
+
+window.mainloop()
+
+
+
+
 
 
 
